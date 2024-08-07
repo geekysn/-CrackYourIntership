a0ffe4bd856dd001f0035916de639276ae1ec0b2
geekysn <155133515+geekysn@users.noreply.github.com>
2024-08-07 22:29:14 +0530

    Day-8

---
+++ Linked_list_cycle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vvi vector<vector<int>>
+#define vvc vector<vector<char>>
+#define pp pair<int, int>
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast)return true;
+        }
+        return false;
+    }
+};
---
+++ Middle_of_linked_list.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vvi vector<vector<int>>
+#define vvc vector<vector<char>>
+#define pp pair<int, int>
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
